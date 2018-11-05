dfc2b9d31d5f839ffc497f611bd2d5a7dd993d78
Tatiana <tmedeiross@globo.com>
2018-11-04 22:11:48 -0200

    animate math

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Animated } from "react-native";
 
+const ballY = new Animated.Value(0);
+const ballX = Animated.divide(ballY, 4);
+
 export default class App extends Component {
   state = {
-    ballY: new Animated.Value(0)
+    ballY: ballY,
+    ballX: ballX
   };
 
   componentDidMount() {
     Animated.decay(this.state.ballY, {
-      velocity: 0.5
+      velocity: 1
     }).start();
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.ball, { top: this.state.ballY }]} />
+        <Animated.View
+          style={[
+            styles.ball,
+            { top: this.state.ballY, left: this.state.ballX }
+          ]}
+        />
       </View>
     );
   }
